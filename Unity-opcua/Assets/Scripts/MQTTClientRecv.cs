7434bf5543734ba49d4deafcfd442344ab96d60b
ZouHeng <zouheng_zhhe@163.com>
2017-12-19 15:57:59 +0800

    2017/12/19,上传opcua客户端，整合四种通讯协议

---
+++ Unity-opcua/Assets/Scripts/MQTTClientRecv.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using UnityEngine;
+
+public class MQTTClientRecv : MonoBehaviour
+{
+    private MqttClient client;
+
+    private void Start()
+    {
+        client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+        string clientId = Guid.NewGuid().ToString();
+        try
+        {
+            client.Connect(clientId);
+        }
+        catch
+        {
+            print("客户端与服务器端连接失败！");
+        }
+        client.MqttMsgPublishReceived += OnReceive;
+        client.Subscribe(new string[] { "opcua" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+    }
+
+    private void OnReceive(object sender, MqttMsgPublishEventArgs e)
+    {
+        print(Encoding.UTF8.GetString(e.Message));
+    }
+
+    private void OnDestroy()
+    {
+        client.Disconnect();
+    }
+}
